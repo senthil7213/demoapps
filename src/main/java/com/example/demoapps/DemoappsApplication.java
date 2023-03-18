c72644f2a3fa9b4563386c556236f0522f37c60f
senthil7213 <senthil7213@gmail.com>
2023-03-18 17:36:27 +0530

    new project added.

---
+++ src/main/java/com/example/demoapps/DemoappsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoapps/DemoappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
